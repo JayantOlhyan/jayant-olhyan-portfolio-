deed372dfbefe6f61d1798dc25c70c3fad9cca6e
JayantOlhyan <jayantolhyan@gmail.com>
2026-04-06 19:54:46 +0530

    chore: add build ignore comment to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  // Ignore distribution build results
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
